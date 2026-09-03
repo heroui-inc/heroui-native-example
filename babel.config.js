89325065e4c00137213f88687412a7804424c94a
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2026-09-03 17:05:37 +0300

    fix: lingui setup

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,9 @@ module.exports = function (api) {
       presets: [
         ["babel-preset-expo"],
       ],
+      plugins: [
+        // Must run first so Lingui macros are expanded before any other transform.
+        "@lingui/babel-plugin-lingui-macro",
+      ],
     };
   };
